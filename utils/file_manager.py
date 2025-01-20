4762c50a16a25b5423c3083f6605c17b09ceaf6a
Stella Bischoff <bischoff.stella3@gmail.com>
2025-01-20 20:45:34 +0000

    functionality to Write list to json and json file

---
+++ utils/file_manager.py
@@ -0,0 +1,5 @@
+import json
+
+def save_to_json_file(data, file_name):
+    with open("data/"+file_name, "w") as outfile:
+        json.dump(data, outfile, indent=4)
--- utils/to_do_list.py
+++ utils/to_do_list.py
@@ -1,5 +1,6 @@
 import json
 from utils.item import ListItem
+from utils.file_manager import save_to_json_file
 
 
 class ToDoList:
@@ -10,7 +11,8 @@ class ToDoList:
         self.name = name
         if items != []:
             self.items = self._convert_item_list(items)
-        #print(json.loads(self.create_json()))
+        
+        self.save_to_file()
 
     def add_item_to_list(self, new_items):
         self.items.extend(new_items)
@@ -26,10 +28,16 @@ class ToDoList:
     def _convert_item(self, item):
         # convert string to list item
         return ListItem(item)
+    #! Lernt something
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "items": [{"item": item.name, "done": item.done} for item in self.items]
+        }
 
+    #! indent?
     def create_json(self):
-        # return a json as string
-        item_str = ""
-        for i in self.items:
-            item_str += f'{{"item":"{i.name}","done":"{i.done}"}},'
-        return f'{{"list": "{self.name}","items":[{item_str}]"}}'
+        return json.dumps(self.to_dict(), indent=4)
+    
+    def save_to_file(self, file_name: str = "to_do_list.json"):
+        save_to_json_file(self.to_dict(), file_name)
